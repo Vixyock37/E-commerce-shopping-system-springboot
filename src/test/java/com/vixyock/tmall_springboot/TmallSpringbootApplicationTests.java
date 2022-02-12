af357db7b1c7f3f09004717429daea7e6afd4393
Vixyock <815135204@qq.com>
2022-02-12 18:08:57 +0800

    version0.1.0 The very beginning  of the project. 仅做好配置，什么都没有。

---
+++ src/test/java/com/vixyock/tmall_springboot/TmallSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vixyock.tmall_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmallSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
