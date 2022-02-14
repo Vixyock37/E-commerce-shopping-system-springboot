7811a5317b38cee00a8708924c45eeb45541eb44
Vixyock <815135204@qq.com>
2022-02-14 22:53:57 +0800

    version0.1.6 在上一版本的基础上对application.properties进行了配置，并提供了静态文件。 为测试提供了10条测试数据，现可进行最基本的获取与展示数据

---
+++ src/main/java/com/vixyock/tmall/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.vixyock.tmall.service;
+
+import com.vixyock.tmall.dao.CategoryDAO;
+import com.vixyock.tmall.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryDAO categoryDAO;
+
+    public List<Category> list() {
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        return categoryDAO.findAll(sort);
+    }
+}
