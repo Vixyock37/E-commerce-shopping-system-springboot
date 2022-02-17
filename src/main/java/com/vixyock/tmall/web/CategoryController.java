50679e6f2568b95a9e9f94f0805c82ba5af71e6e
Vixyock <815135204@qq.com>
2022-02-17 18:14:59 +0800

    version2.1.1 Vixyock 增加功能 修改了listCategory.html +:data4vue增加了bean属性，用来存放分类信息，增加了file属性，用来表示上传的文件 +:提供getFile函数，获取文件并保存在data4Vue.file上 +:提供add函数 +:新增了新增div 修改了CategoryService +:增加add方法 新增了ImageUtil工具类 修改了CategoryService +:增加方法add

--- src/main/java/com/vixyock/tmall/web/CategoryController.java
+++ src/main/java/com/vixyock/tmall/web/CategoryController.java
@@ -2,12 +2,20 @@ package com.vixyock.tmall.web;
 
 import com.vixyock.tmall.pojo.Category;
 import com.vixyock.tmall.service.CategoryService;
+import com.vixyock.tmall.util.ImageUtil;
 import com.vixyock.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,15 +24,33 @@ public class CategoryController {
     CategoryService categoryService;
 
     @GetMapping("/categories")
-//    public List<Category> list() throws Exception{
-//        System.out.println(categoryService.list());
-//        return categoryService.list();
-//    }
     public Page4Navigator<Category> list(@RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
         start = start < 0 ? 0 : start;
         Page4Navigator<Category> page = categoryService.list(start, size, 5);
         //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
 //        System.out.println(page.toString());
         return page;
+    }
+    //    public List<Category> list() throws Exception{
+//        System.out.println(categoryService.list());
+//        return categoryService.list();
+//    }
+
+    @PostMapping("/categories")
+    public Object add(Category bean, MultipartFile image, HttpServletRequest request) throws IOException {
+        categoryService.add(bean);
+        saveOrUpadateImageFile(bean, image, request);
+        return bean;
+    }
+
+    public void saveOrUpadateImageFile(Category bean, MultipartFile image, HttpServletRequest request)
+            throws IOException {
+        File imageFolder = new File(request.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder, bean.getId() + ".jpg");
+        if (!file.getParentFile().exists())
+            file.getParentFile().mkdirs();
+        image.transferTo(file);
+        BufferedImage img = ImageUtil.change2jpg(file);
+        ImageIO.write(img, "jpg", file);
     }
 }
